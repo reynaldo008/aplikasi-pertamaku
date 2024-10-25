d88878d0cc02f974da104c22b2b1a73c700ede3d
reynaldo008 <128881216+reynaldo008@users.noreply.github.com>
2024-10-25 16:37:06 +0700

    Perbaikan Code
    sudah di commit.

--- frontend/src/components/CommentSection.vue
+++ frontend/src/components/CommentSection.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ref } from 'vue';
+import DOMPurify from 'dompurify'; // Import DOMPurify untuk sanitasi
 
 const comment = ref('');
 const comments = ref('');
 
+// Gunakan DOMPurify untuk membersihkan input sebelum menambahkannya ke comments
 const submitComment = () => {
-  comments.value += `<p>${comment.value}</p>`;
+  const sanitizedComment = DOMPurify.sanitize(`<p>${comment.value}</p>`);
+  comments.value += sanitizedComment;
   comment.value = '';
 };
 </script>
@@ -15,6 +18,7 @@ const submitComment = () => {
     <h3>Comments</h3>
     <input v-model="comment" placeholder="Leave a comment" />
     <button @click="submitComment">Submit</button>
+    <!-- comments di-render menggunakan v-html, pastikan input telah disanitasi -->
     <div v-html="comments"></div>
   </div>
 </template>
